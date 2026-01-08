def9602d28f260d9aa72605b9bb659ea2937d948
gudwns1812 <gudwns1812@naver.com>
2026-01-08 20:28:21 +0900

    init

---
+++ src/main/java/com/datecourse/DatecourseApplication.java
@@ -0,0 +1,13 @@
+package com.datecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatecourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatecourseApplication.class, args);
+    }
+
+}
