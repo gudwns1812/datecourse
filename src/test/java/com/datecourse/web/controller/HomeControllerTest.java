90d4013787086ac38065e40f39c76acba425bf54
gudwns1812 <gudwns1812@naver.com>
2026-01-20 20:43:14 +0900

    test: HomeControllerTest 추가

---
+++ src/test/java/com/datecourse/web/controller/HomeControllerTest.java
@@ -0,0 +1,56 @@
+package com.datecourse.web.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.datecourse.domain.member.Member;
+import com.datecourse.repository.MemberRepository;
+import com.datecourse.web.constrant.SessionConst;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean // 가짜 빈으로 등록하여 의존성 해결!
+    MemberRepository memberRepository;
+
+    @Test
+    void argumentResolverTest() throws Exception {
+        //given
+        Member memberTest = Member.builder()
+                .loginId("test")
+                .password("test!")
+                .email("test123@gmail.com")
+                .gender("M")
+                .username("테스터")
+                .phoneNumber("010-1234-5678")
+                .build();
+
+        given(memberRepository.findById(1L)).willReturn(Optional.of(memberTest));
+        //when
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(SessionConst.MEMBER_ID, 1L);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(session);
+        //then
+        mockMvc.perform(get("/")
+                        .session(session)) // 생성한 가짜 세션을 요청에 포함
+                .andExpect(status().isOk())
+                .andExpect(view().name("loginHome"))
+                .andDo(print());
+    }
+}
